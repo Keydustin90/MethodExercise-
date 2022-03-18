71e5bf24b1bcdb24f8776e8c342f7d36453d86ae
Dustin Key <Keydustin90@gmail.com>
2022-03-18 15:07:52 -0500

    finished MethodExercise

---
+++ MethodExercise/MethodExercise/MyClass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace MethodExercise
+{
+    public class MyClass
+
+    public static int Sum (int num1, int num2 )
+    {
+        return = num1 + num2
+    }
+            
+        
+    public static int multiply(int num1, int num2, int num3)
+    {
+        return = num1 * num2 * num3
+    }
+
+    public static int Subtract(int num1, int num2)
+    {
+        return = num1 - num2 
+    }
+
+    public static int Divde(int num1, int num2)
+    {
+        return = num1 \ num2
+    }
+
+    public static int modulus(int num1, int num2)
+    {
+        return = num1 % num2
+        }
+    static void Main(string[]args)
+
+            var amountOfCar = sum(2, 6);
+            var blah = Multiply(20, 4, 6);
+            var goodsDown = subtract(6, 3);
+            var haftOf = divde(8, 4);
+            var remainderOf = modulus(9, 2);
+
+       
+        {
+           /// -------- Exercise 1 
+
+            Console.WriteLine("Hello, what is your name?");
+
+            var userName = Console.ReadLine();
+
+            Console.WriteLine("Hey {userName}. what is your favorite Color?");
+
+            var color = Console.ReadLine();
+
+            Console.WriteLine("{color} is an great color! what is your favorite animal?");
+
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Awesome!! what is your favorite band?");
+
+            var band = Console.ReadLine();
+
+            Console.WriteLine($"Thank you, {userName}! Here is your profile");
+            Console.WriteLine($"-------------")
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Favorite color: {color}");
+            Console.WriteLine($"Favorite animal: {animal}");
+            Console.WriteLine($"Favorite band: {band}");
+
+            /// ------- Exercise 2
+
+        }
+
+    }
+}
